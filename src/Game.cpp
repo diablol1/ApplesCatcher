2f89607b395ec23bdfcc6ca8f9b4024f328e8642
diablol1 <patryk.krajewski002@gmail.com>
2016-12-23 14:10:07 +0100

    Added walls

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,9 +4,35 @@ const sf::Vector2i Game::ScreenResolution = sf::Vector2i(800, 600);
 
 Game::Game() : window(sf::VideoMode(ScreenResolution.x, ScreenResolution.y), "Apples Catcher")
 {
+	initWalls();
+
 	loadTextures();
-	player = Player(playerTexture, sf::Vector2f(ScreenResolution.x / 2, ScreenResolution.y - playerTexture.getSize().y));
+	player = Player(playerTexture, sf::Vector2f(ScreenResolution.x / 2,
+		ScreenResolution.y - playerTexture.getSize().y - walls["bottomWall"].getSize().y));
 }
+
+void Game::loadTextures()
+{
+	if (!playerTexture.loadFromFile("sprites/tmpPlayer.png"))
+		printf("Encountered error while loading texture.");
+}
+
+void Game::initWalls()
+{
+	walls["leftWall"].setSize(sf::Vector2f(30, ScreenResolution.y));
+	walls["rightWall"].setSize(sf::Vector2f(30, ScreenResolution.y));
+	walls["bottomWall"].setSize(sf::Vector2f(ScreenResolution.x, 30));
+
+	walls["leftWall"].setPosition(0, 0);
+	walls["rightWall"].setPosition(ScreenResolution.x - walls["rightWall"].getSize().x, 0);
+	walls["bottomWall"].setPosition(0, ScreenResolution.y - walls["bottomWall"].getSize().y);
+
+	for (auto &wall : walls)
+	{
+		wall.second.setFillColor(sf::Color::Magenta);
+	}
+}
+
 
 void Game::start()
 {
@@ -44,12 +70,10 @@ void Game::render()
 	window.clear();
 
 	window.draw(player);
+	for (auto &wall : walls)
+	{
+		window.draw(wall.second);
+	}
 	
 	window.display();
-}
-
-void Game::loadTextures()
-{
-	if (!playerTexture.loadFromFile("sprites/tmpPlayer.png"))
-		printf("Encountered error while loading texture.");
 }
--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
 
 #include "Player.h"
 
@@ -14,11 +16,13 @@ private:
 	static const sf::Vector2i ScreenResolution;
 	sf::RenderWindow window;
 
+	std::map<std::string, sf::RectangleShape> walls;
 	Player player;
 	sf::Texture playerTexture;
 
 	void loadTextures();
-
+	void initWalls();
+	
 	void processEvents();
 	void update(const float &deltaTime);
 	void render();
